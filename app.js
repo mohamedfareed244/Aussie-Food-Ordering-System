52d75b528e0d2afcbb0e08a3ea39125d25e6b076
mohamed fareed <125537609+mohamedfareed244@users.noreply.github.com>
2023-05-18 00:46:38 +0300

    new app.js file with routes handling and export app for www.js

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import ejs, { compile } from "ejs";
+import mongoose from "mongoose"
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+//import the routes 
+import product_router from "./routes/products.js";
+
+
+//import the routes 
+import {All} from "./models/schema.js";
+import {Sec} from "./models/menu_sections.js";
+import { CLIENT_RENEG_LIMIT } from "tls";
+import session from "express-session";
+import cors from "cors"
+//Read the current directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//set up app 
+const app = express();
+//set up session 
+app.use(session({ secret: 'Your_Secret_Key' }));
+
+
+app.set('view engine', 'ejs');
+//setup static 
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+
+//routes 
+app.use('/products',product_router);
+
+
+//test playing sound for use in the future at orders 
+app.get("/Users/user/Downloads/Message%20notification.m4r",function(req,res){
+    res.sendFile(path.join("/Users/user/Downloads/Message notification.m4r"));
+})
+
+
+//error handling 
+app.use((req,res)=>{
+    res.send("OOPS! it seems that there are an error try again with a valid URL ");
+})
+
+export default app;
+
+
+
+
+
+
+
+
+
+
+
+
+
