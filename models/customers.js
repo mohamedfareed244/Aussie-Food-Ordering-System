1f2e661bf5d153410e53b3daf229ace29c23081b
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-05-18 16:13:24 +0300

    edit customers schema

--- models/customers.js
+++ models/customers.js
@@ -2,17 +2,16 @@ import { Double } from 'mongodb';
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const customers = new Schema({
-customerid:String,
-orders:[{orderid : String}],
-firstname: String,
-middlename: String,
-lastname: String,
-phone:String,
-email:String,
-psw: String
+const Custom = new Schema({
+Firstname:String,
+Middlename:String,
+Lastname:String,
+Phone:Number,
+Orders:[{id:String}],
+Email:String,
+Password:String,
   
 });
- const customerss =mongoose.model('customerss',customers);
+ const customers =mongoose.model('customers',Custom);
 
-export {customerss};
+export {customers};
