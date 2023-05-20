9b386b4a0593ddde32f90284649b0c988a53e6df
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-05-21 02:54:22 +0300

    add adresses array to customers

--- models/customers.js
+++ models/customers.js
@@ -11,6 +11,7 @@ Orders:[{id:String}],
 Email:String,
 Password:String,
 chat:[{msg:String, issent:Boolean}],
+addreses:[{ location: String, Adress:String, apartement: string, floor: String, Building:String}],
   
 });
  const customers =mongoose.model('customers',Custom);
