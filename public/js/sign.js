ee8bb2b5818ead0e570c27e9e3d1913ce90e129b
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-05-20 15:46:27 +0300

    add choose photo script

--- public/js/sign.js
+++ public/js/sign.js
@@ -40,3 +40,17 @@ function validate(form)
         return false;
     }
 }
+
+
+
+const display = document.querySelector(".display");
+const input = document.querySelector("#upload");
+let img = document.querySelector("img");
+
+input.addEventListener("change", () => {
+  let reader = new FileReader();
+  reader.readAsDataURL(input.files[0]);
+  reader.addEventListener("load", () => {
+    display.innerHTML = `<img src=${reader.result} alt=''/>`;
+  });
+});
