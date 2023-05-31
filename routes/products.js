68f81b32fc2f2d1cd4e8f6b28e417d4bb3ae72b9
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-05-31 17:23:50 +0300

    add a post method to add section form

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 import Router from "express"
 //import the products databse controllers 
-import {getsection} from "../controllers/products_controller.js";
+import {getsection, postsection} from "../controllers/products_controller.js";
 import {getitembyid} from "../controllers/products_controller.js";
 import {delitem} from "../controllers/products_controller.js";
 import {check_out} from "../controllers/products_controller.js";
 import {new_order} from "../controllers/products_controller.js";
+// import {postsection} from "../controllers/products_controller.js";
 //import the products database controllers 
 
 
@@ -18,7 +19,8 @@ router.get('/getitem/:id',getitembyid);
 router.get('/cartdel/:id',delitem);
 //check out 
 router.get('/cart/end/checkout',check_out);
-//
+//add section
+router.get('/add section',postsection);
 //router.post('/customer/order/address',)
 router.get('/customer/cart/checkout/order',new_order);
 export default router;
