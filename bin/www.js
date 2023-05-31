8cf555512edaa13bfaabefe8767f59470923cbac
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-05-31 21:31:48 +0300

    connect customer with the admin and wait

--- bin/www.js
+++ bin/www.js
@@ -66,7 +66,7 @@ if(!finded){
   await getmyemp(sess.signed_customer).then( (o)=>{
     console.log("get my emp returned with ",o);
 io.to(o).emit("newcustomer",{"customer":sess.signed_customer});
-io.to(socket.id).emit("connects_emp",{"kk":"ss"});
+io.to(socket.id).emit("connects_emp",{"name":"mohamed fareed"});
   })
   
 }
