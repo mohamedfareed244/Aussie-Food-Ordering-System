4b9592feb0439518c6eef5249bffa9271d0b269e
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-06-01 14:55:10 +0300

    css for wait icon

--- public/js/sign.js
+++ public/js/sign.js
@@ -29,20 +29,9 @@ function validatepassword(field)
 }
 function validate()
 {
+    console.log("i join ")
     document.getElementById("waitsign").style.display="block";
  
 }
 
 
-
-const display = document.querySelector(".display");
-const input = document.querySelector("#upload");
-let img = document.querySelector("img");
-
-input.addEventListener("change", () => {
-  let reader = new FileReader();
-  reader.readAsDataURL(input.files[0]);
-  reader.addEventListener("load", () => {
-    display.innerHTML = `<img src=${reader.result} alt=''/>`;
-  });
-});
