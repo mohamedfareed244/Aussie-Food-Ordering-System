1dd1857fdb60438477422e05aca3c104d842c195
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 19:46:48 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,28 +1,29 @@
 import Router from "express"
 //import the products databse controllers 
-import {getsection, postproduct, postsection} from "../controllers/products_controller.js";
-import {getitembyid} from "../controllers/products_controller.js";
-import {delitem} from "../controllers/products_controller.js";
-import {check_out} from "../controllers/products_controller.js";
-import {new_order} from "../controllers/products_controller.js";
+import { getsection, postproduct, postsection } from "../controllers/products_controller.js";
+import { getitembyid } from "../controllers/products_controller.js";
+import { delitem } from "../controllers/products_controller.js";
+import { check_out } from "../controllers/products_controller.js";
+import { new_order } from "../controllers/products_controller.js";
 // import {postsection} from "../controllers/products_controller.js";
 //import the products database controllers 
 
 
-const router=Router();
+const router = Router();
 
 //get section items by section name 
-router.get('/:sec_name',getsection);
+router.get('/:sec_name', getsection);
 //send item id to append to the cart 
-router.get('/getitem/:id',getitembyid);
+router.get('/getitem/:id', getitembyid);
 //delete item from the cart 
-router.get('/cartdel/:id',delitem);
+router.get('/cartdel/:id', delitem);
 //check out 
-router.get('/cart/end/checkout',check_out);
+router.get('/cart/end/checkout', check_out);
 //add section
-router.get('/add section',postsection);
+router.get('/add section', postsection);
 //add product
-router.get('/add section',postproduct);
+router.get('/add section', postproduct);
 //router.post('/customer/order/address',)
-router.get('/customer/cart/checkout/order',new_order);
+router.get('/customer/cart/checkout/order', new_order);
 export default router;
+//formated
