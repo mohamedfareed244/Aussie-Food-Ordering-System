cc3f48e9186d6c15ded75ff95e39ae8e6a747b93
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 19:46:12 +0300

    Update employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -1,29 +1,29 @@
 
 import Router from "express"
-import {getemployees} from "../controllers/employees-controllers.js";
-import {postemployees} from "../controllers/employees-controllers.js";
-import {confirmmail} from "../controllers/employees-controllers.js";
-import {empprof} from "../controllers/employees-controllers.js";
-import {changepass} from "../controllers/employees-controllers.js";
-import {getallchats} from "../controllers/employees-controllers.js";
-import {getallchatssel} from "../controllers/employees-controllers.js";
-const router=Router();
+import { getemployees } from "../controllers/employees-controllers.js";
+import { postemployees } from "../controllers/employees-controllers.js";
+import { confirmmail } from "../controllers/employees-controllers.js";
+import { empprof } from "../controllers/employees-controllers.js";
+import { changepass } from "../controllers/employees-controllers.js";
+import { getallchats } from "../controllers/employees-controllers.js";
+import { getallchatssel } from "../controllers/employees-controllers.js";
+const router = Router();
 console.log(postemployees);
 //employees sign up 
- router.post("/", postemployees)
+router.post("/", postemployees)
 //employees sign in 
-  router.post("/signin", getemployees);
+router.post("/signin", getemployees);
 //access to employees profile 
-router.get("/profile",empprof);
+router.get("/profile", empprof);
 //verify employees mail 
-router.get("/mail/verification/:id",confirmmail);
+router.get("/mail/verification/:id", confirmmail);
 //change employees password 
-router.post("/profile/password/change",changepass);
-router.get("/signin",(req,res)=>{
-  res.render("admin_signin",{alert:false});
+router.post("/profile/password/change", changepass);
+router.get("/signin", (req, res) => {
+  res.render("admin_signin", { alert: false });
 })
-router.get("/profile/chat/details/:id",getallchatssel);
-router.get("/profile/chat/details",getallchats);
+router.get("/profile/chat/details/:id", getallchatssel);
+router.get("/profile/chat/details", getallchats);
 // app.get('/', (req, res) => 
 // {
 //   res.render('index')
@@ -38,4 +38,5 @@ router.get("/profile/chat/details",getallchats);
 
 
 
- export default router;
+export default router;
+//formated
