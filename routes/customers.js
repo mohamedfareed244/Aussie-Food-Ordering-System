948a54a26888a943a29edc56c47d4ae5b6406b5c
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 19:43:34 +0300

    Update customers.js format

--- routes/customers.js
+++ routes/customers.js
@@ -1,30 +1,31 @@
 
 import Router from "express"
-import {getcustomers} from "../controllers/customers-controller.js";
-import {postcustomers} from "../controllers/customers-controller.js";
-import {customerpr} from "../controllers/customers-controller.js";
-import {customeror} from "../controllers/customers-controller.js";
-import {customerml} from "../controllers/customers-controller.js";
-import {customeraddr} from "../controllers/customers-controller.js";
-import {customerfav} from "../controllers/customers-controller.js";
-import {customerchnagepass} from "../controllers/customers-controller.js";
-import {getmsgs} from "../controllers/customers-controller.js";
-const router=Router();
+import { getcustomers } from "../controllers/customers-controller.js";
+import { postcustomers } from "../controllers/customers-controller.js";
+import { customerpr } from "../controllers/customers-controller.js";
+import { customeror } from "../controllers/customers-controller.js";
+import { customerml } from "../controllers/customers-controller.js";
+import { customeraddr } from "../controllers/customers-controller.js";
+import { customerfav } from "../controllers/customers-controller.js";
+import { customerchnagepass } from "../controllers/customers-controller.js";
+import { getmsgs } from "../controllers/customers-controller.js";
+const router = Router();
 
-router.post('/signup', postcustomers );
-  router.post("/signin", getcustomers);
+router.post('/signup', postcustomers);
+router.post("/signin", getcustomers);
 
-  router.get('/signup',async (req,res)=>{
-    res.render("register",{alert:false});
-  });
-  router.get('/signin',async (req,res)=>{
-    res.render("sign-in",{alert:false});
-  });
-  router.get('/profile/login',customerpr);
-  router.post('/profile/login/account/changepass',customerchnagepass);
-  router.get('/profile/orders',customeror);
-router.get('/profile/favs',customerfav);
-  router.get('/mail/validation/:id',customerml);
-  router.get('/profile/addr',customeraddr);
-  router.get('/admin/chat/changeuser/:id',getmsgs);
-  export default router;
+router.get('/signup', async (req, res) => {
+  res.render("register", { alert: false });
+});
+router.get('/signin', async (req, res) => {
+  res.render("sign-in", { alert: false });
+});
+router.get('/profile/login', customerpr);
+router.post('/profile/login/account/changepass', customerchnagepass);
+router.get('/profile/orders', customeror);
+router.get('/profile/favs', customerfav);
+router.get('/mail/validation/:id', customerml);
+router.get('/profile/addr', customeraddr);
+router.get('/admin/chat/changeuser/:id', getmsgs);
+export default router;
+//formated
