20c3d7dda68b5e25bfd69a17a994c35ea8a7261d
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 17:31:16 +0300

    Update customers.js

--- models/customers.js
+++ models/customers.js
@@ -3,19 +3,20 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const Custom = new Schema({
-Firstname:String,
-Middlename:String,
-Lastname:String,
-Phone:String,
-Orders:[{id:String}],
-Email:String,
-Password:String,
-chat:[{msg:String, issent:Boolean}],
-addreses:[{ location: String, Adress:String, apartement: String, floor: String, Building:String}],
-favorites:[{ itemId: String}],
-verified:Boolean
-  
-});
- const customers =mongoose.model('customers',Custom);
+    Firstname: String,
+    Middlename: String,
+    Lastname: String,
+    Phone: String,
+    Orders: [{ id: String }],
+    Email: String,
+    Password: String,
+    chat: [{ msg: String, issent: Boolean }],
+    addreses: [{ location: String, Adress: String, apartement: String, floor: String, Building: String }],
+    favorites: [{ itemId: String }],
+    verified: Boolean
 
-export {customers};
+});
+const customers = mongoose.model('customers', Custom);
+
+export { customers };
+//formated
