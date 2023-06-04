36e25ae8f9908463e2acb6b61ffc4408897ffcea
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 17:32:52 +0300

    Update menu_sections.js format

--- models/menu_sections.js
+++ models/menu_sections.js
@@ -2,11 +2,12 @@ import { Double } from 'mongodb';
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const Sections= new Schema({
- name:String,
-items:[{id:String}]
-  
-});
-const Sec=mongoose.model('Sec',Sections);
+const Sections = new Schema({
+    name: String,
+    items: [{ id: String }]
 
-export {Sec};
+});
+const Sec = mongoose.model('Sec', Sections);
+
+export { Sec };
+//formated
