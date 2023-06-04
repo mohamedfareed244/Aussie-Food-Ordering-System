b3d269c37877d74288580a66d2f538aef508fb95
yassinsultan6 <125744343+yassinsultan6@users.noreply.github.com>
2023-06-04 19:35:02 +0300

    Update sign.js

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,36 +1,31 @@
-function validateemail(field)
-{
-    if(field==''){
-        document.getElementById('mail').innerHTML='Please enter your email adress';
+function validateemail(field) {
+    if (field == '') {
+        document.getElementById('mail').innerHTML = 'Please enter your email adress';
         document.getElementById('idmail').style.borderColor = "red";
         return false;
     }
-    
+
 }
-function validatepassword(field)
-{
-    if(field==''){
-        document.getElementById('password').innerHTML='Please enter your password';
+function validatepassword(field) {
+    if (field == '') {
+        document.getElementById('password').innerHTML = 'Please enter your password';
         document.getElementById('idpass').style.borderColor = "red";
         return false;
     }
-    
+
 }
-function validate(form)
-{
-    let fail='';
-    fail&=validateemail(form.idmail.value);
-    fail&=validateMName(form.idpass.value);
-    if(fail)
-    {
+function validate(form) {
+    let fail = '';
+    fail &= validateemail(form.idmail.value);
+    fail &= validateMName(form.idpass.value);
+    if (fail) {
         return true;
     }
-        
-    else
-    {
+
+    else {
         return false;
     }
- 
-}
 
+}
+//formated
 
