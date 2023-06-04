c5fa147d336b8bc2902a1cd172a38ef2e65f9400
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-06-04 06:48:42 +0300

    add a pop up box to edit item

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.get("/employees/profile/customers",function(req,res){
   res.render("recent-customers");
 })
    app.get("/emp",function(req,res){
-     res.render("dashboard-employees");
+     res.render("admin-dashboard-menu");
 })
 
 //error handling 
