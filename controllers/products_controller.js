6a252b30c49bd4cc5487de3d5d79b215fabe65cb
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-06-05 07:53:34 +0300

    add comments section to the pop up div

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -177,7 +177,8 @@ const new_order = async (req, res) => {
   await rec_order(emp, ord);
   const obj = { num: ord.num, confirm: true };
   req.session.cart_items = new Array();
-  res.render("check_out", { cart: req.session.cart_items, user: req.session.signed_customer, ord: obj });
+  res.json(obj);
+  // res.render("check_out", { cart: req.session.cart_items, user: req.session.signed_customer, ord: obj });
 }
 
 
