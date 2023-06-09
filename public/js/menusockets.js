8904d4772b17d8c3fdf3a995477e408af5f8c34a
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2023-06-09 08:48:25 +0300

    add file menu sockets for the admin to recieve orders and chats every where

---
+++ public/js/menusockets.js
@@ -0,0 +1,58 @@
+var socket = io();
+socket.on('connect', () => {
+  console.log("socket id " + socket.id);
+})
+
+var form = document.getElementById("form");
+var input = document.getElementById("input");
+
+socket.on('recieve message', async (msg) => {
+  console.log("i recived message with ", msg);
+  sou1();
+})
+
+socket.on('recieve order', async (msg) => {
+  displayalert("You have recieved a new order");
+  console.log("recieve", msg);
+  sou();
+
+})
+socket.on("require signin", () => {
+  location.href = '/employees/signin';
+})
+socket.on("newcustomer", async (msg) => {
+  displayalert("New customer connected to chat ");
+  sou1();
+  console.log("i recieve ", msg.customer.Firstname);
+})
+socket.on("getmessage", async (msg) => {
+  console.log("msg from ", msg.from);
+  displayalert(`New message from ${msg.from}`);
+  sou1();
+})
+async function displayalert(text) {
+  document.getElementById("almsg").style.display="block";
+  document.getElementById("almsg").innerHTML = `${text}`
+ setTimeout(hidealert,3000);
+
+}
+async function hidealert() {
+  document.getElementById("alylert").style.display= "none";
+
+}
+async function sou() {
+
+  let audio = document.getElementById("audio");
+  audio.play().catch((err) => {
+    console.log(err);
+  })
+
+}
+async function sou1() {
+
+  let audio = document.getElementById("audio1");
+  audio.play().catch((err) => {
+    console.log(err);
+  })
+
+}
